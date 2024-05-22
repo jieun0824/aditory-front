e6503fd3f0a966ded9c1b6fb3354be4566ba449d
jieun <suspen064@gmail.com>
2024-05-22 19:44:58 +0900

    fix: fix button design

--- src/app/(after-login)/(without-logo)/_component/link-detail-component.tsx
+++ src/app/(after-login)/(without-logo)/_component/link-detail-component.tsx
@@ -10,7 +10,11 @@ import {
 import { Separator } from '@/components/ui/separator';
 import { useAccessToken } from '@/lib/useAccessToken';
 import { useLink } from '@/service/links/useLinkService';
-import { Suspense, useEffect } from 'react';
+import { Suspense, useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import { Button } from '@/components/ui/button';
+import { CiEdit, CiExport, CiTurnR1 } from 'react-icons/ci';
+import Link from 'next/link';
 
 export default function LinkDetailComponent({ linkId }: { linkId: number }) {
   const { accessToken, getRefreshToken } = useAccessToken();
@@ -18,25 +22,35 @@ export default function LinkDetailComponent({ linkId }: { linkId: number }) {
     accessToken: accessToken,
     linkId: linkId,
   });
+  const createdDate = dayjs(data?.data.createdAt).format('YYYY-MM-DD');
+  const nowDate = new Date();
+  const [editMode, setEditMode] = useState(false); //false->view, true->edit
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Card className='h-full w-full'>
         <CardHeader>
-          <CardDescription></CardDescription>
+          <CardDescription>{createdDate}</CardDescription>
           <CardTitle>{data?.data.title}</CardTitle>
         </CardHeader>
         <CardContent className=''>{data?.data.summary}</CardContent>
         <Separator />
-        <CardFooter className='flex justify-between'>
-          <div className=''>
-            <iframe
-              className='relative inset-0 top-1 z-40 rounded-md border'
-              sandbox='allow-modals allow-forms allow-popups allow-scripts allow-same-origin'
-              src={data?.data.url}
-              width='100%'
-              height='200'
-              title='Link Preview'
-            />
+        <CardFooter className='flex w-full flex-col justify-between gap-3'>
+          <iframe
+            className='relative inset-0 top-1 z-40 rounded-md border'
+            sandbox='allow-modals allow-forms allow-popups allow-scripts allow-same-origin'
+            src={data?.data.url}
+            width='100%'
+            height='200'
+            title='Link Preview'
+          />
+          <div className='flex gap-3'>
+            <Button variant={'outline'}>
+              Link <CiExport className='ml-2' />
+            </Button>
+
+            <Button variant={'outline'}>
+              Edit <CiEdit className='ml-2' />
+            </Button>
           </div>
         </CardFooter>
       </Card>
--- src/app/(after-login)/(without-logo)/layout.tsx
+++ src/app/(after-login)/(without-logo)/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className='relative flex min-h-dvh w-screen flex-col items-center justify-center'>
       <Header />
-      <main className='flex h-full w-full max-w-md flex-col items-center justify-center gap-6 px-4 py-16'>
+      <main className='flex h-full w-full max-w-md flex-col items-center justify-center gap-6 py-16'>
         {children}
       </main>
       <NavBar />
