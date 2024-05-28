c1cbc2e75073a7203fdeb2db213bff4b7cbc283c
jieun <suspen064@gmail.com>
2024-05-28 22:02:35 +0900

    fix: remove moreButton in linkId pages

--- src/app/(after-login)/(without-logo)/_component/header.tsx
+++ src/app/(after-login)/(without-logo)/_component/header.tsx
@@ -1,11 +1,12 @@
 'use client';
 import { ModeToggle } from '@/components/mode-toggle';
-import { usePathname, useRouter } from 'next/navigation';
+import { useParams, usePathname, useRouter } from 'next/navigation';
 import { IoIosArrowBack } from 'react-icons/io';
 import MoreButton from './more-button';
 
 export default function Header() {
   const router = useRouter();
+  const param = useParams();
 
   return (
     <div className='absolute top-0 flex h-12 w-full items-center justify-between px-4'>
@@ -14,7 +15,7 @@ export default function Header() {
         className='cursor-pointer hover:opacity-60'
         size={30}
       />
-      <MoreButton />
+      {Object.keys(param)[0] === 'categoryId' && <MoreButton />}
     </div>
   );
 }
