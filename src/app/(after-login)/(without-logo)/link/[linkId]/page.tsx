a413f22668593037c9389b708e9358c17c72857f
jieun <suspen064@gmail.com>
2024-06-11 23:33:51 +0900

    fix: add use client

--- src/app/(after-login)/(without-logo)/link/[linkId]/page.tsx
+++ src/app/(after-login)/(without-logo)/link/[linkId]/page.tsx
@@ -6,8 +6,9 @@ export default function LinkDetailPage({
 }: {
   params: { linkId: string };
 }) {
-  return;
-  <ErrorBoundary>
-    <LinkDetailComponent linkId={parseInt(params.linkId)} />;
-  </ErrorBoundary>;
+  return (
+    <ErrorBoundary>
+      <LinkDetailComponent linkId={parseInt(params.linkId)} />
+    </ErrorBoundary>
+  );
 }
